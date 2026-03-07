58ad09de881e548c6b8da48f49b6cc5021985041
Raj Janrao <rajjanrao@Rajs-MacBook-Air.local>
2026-03-08 02:54:34 +0530

    Subscribe section completed successfully

--- homepage/src/components/Subscribe/Subscribe.jsx
+++ homepage/src/components/Subscribe/Subscribe.jsx
@@ -7,17 +7,19 @@ const Subscribe = () => {
             <div className="supporter-div">
                 <p className="supporter-head">Become a supporter and help us on our Journey</p>
                 <button className="supporter-button">Read more about how to <span className="subscribe-arrow-div"></span></button>
-
             </div>
 
             <div className="subscribe-div">
-            <h1 className="subscribe-head">Subscribe to our Newsletter</h1>
-            <p className="subscribe-para">If you want to follow our progress on developing what Europeans want and need, done by Europeans.</p>
-            <div className="input-div">
-                <input type="email" placeholder="Enter your email" className="email-input"/>
-                <button className="subscribe-button">Subscribe</button>
+                <div className="subscribe-text-div">
+                    <h1 className="subscribe-head">Subscribe to our Newsletter</h1>
+                    <p className="subscribe-para">If you want to follow our progress on developing what Europeans want and need, done by Europeans.</p>
+                </div>
+
+                <div className="input-div">
+                    <input type="email" placeholder="Enter your email" className="email-input"/>
+                    <button className="subscribe-button">Subscribe to our newsletter</button>
+                </div>
             </div>
-        </div>
 
         </div>
         
